bb5a726e8eda7397aaa6a7a5b3955923ca482f65
certifiedloud <certifiedloud@gmail.com>
2015-05-06 06:47:42 -0600

    enforce uniqueness on quotes

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class QOTD(models.Model):
-    quote = models.TextField()
+    quote = models.TextField(unique=True)
     said_by = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 

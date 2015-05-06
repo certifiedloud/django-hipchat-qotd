6ce0319f8f176118ef624e7e79bf615f5dd50a8e
certifiedloud <certifiedloud@gmail.com>
2015-05-06 06:20:06 -0600

    fixed some mistakes

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class QUOTD(models.model):
+class QUOTD(models.Model):
     quote = models.TextField()
-    said_by = models.CharField(max+length=200)
+    said_by = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
